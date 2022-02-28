3fa76b73a525fb9d1ef494b84163339b6909b2e3
JolyGolden <nazik00696@mail.ru>
2022-02-28 23:22:28 +0600

    header, hero, projects

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,45 @@ import { DiCssdeck } from 'react-icons/di';
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a style={{display: "flex", alignItems: "center", color: "white", marginBottom: '20px'}}>
+            <DiCssdeck size="3rem" /> <Span>Re:Dead</Span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+        <li>
+            <Link href="#projects">
+                <NavLink>Projects</NavLink>
+            </Link>
+        </li>
+        <li>
+            <Link href="#tech">
+                <NavLink>Technologies</NavLink>
+            </Link>
+        </li>
+        <li>
+            <Link href="#aboutus">
+                <NavLink>About Us</NavLink>
+            </Link>
+        </li>
+    </Div2>
+    <Div3>
+        <SocialIcons href="https://github.com">
+            <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://instagram.com">
+            <AiFillInstagram size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://linkedin.com">
+            <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+    </Div3>
+
+
+  </Container>
 );
 
 export default Header;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,20 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+            Welcome to  <br />
+            Our portfolio.
+
+        </SectionTitle>
+        <SectionText>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid atque corporis deserunt dolorum eum explicabo facilis fugiat minima neque non, officiis reiciendis similique soluta tempora tempore, unde veritatis voluptas!
+        </SectionText>
+        <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+      </LeftSection>
+  </Section>
 );
 
 export default Hero;
